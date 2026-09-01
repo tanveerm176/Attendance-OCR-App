e75d0c5cae1fa947ce647fd8252c0c3214965fe3
Muhammad Tanveer <tanveerm176@gmail.com>
2026-09-01 10:25:51 -0400

    add cell_student_name.png && ocr_test

--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,6 +1,16 @@
 import numpy as np
 import pytest
+from pathlib import Path
 from ocr_pipeline.ocr import preprocess_for_ocr, tesseract_ocr
+import cv2
 
 # Pass in image of text, verify str matches 
 # Pass in image of text, verify that output of preprocess is image (np.ndarray)
+
+SAMPLE_NAME_SCAN = Path("tests/sample_scans/cell_student_name.png")
+
+def test_img_to_str():
+    img_array = cv2.imread(SAMPLE_NAME_SCAN)
+    assert img_array is not None
+    ocr_name = tesseract_ocr(img_array)
+    assert isinstance(ocr_name, str)
