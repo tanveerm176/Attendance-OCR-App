00d61df59973442db91c7b07ee550496b30c7174
Muhammad Tanveer <tanveerm176@gmail.com>
2026-09-02 14:28:41 -0400

    extended config.py with PipelineConfig dataclass

--- config.py
+++ config.py
@@ -1,5 +1,21 @@
 import os
+from dataclasses import dataclass
 
 DPI = int(os.getenv("DPI", 300))
 BUCKET_NAME = os.getenv("S3_BUCKET", None)   # None = local mode
 OUTPUT_DIR  = os.getenv("OUTPUT_DIR", "./output")
+
+@dataclass
+class PipelineConfig:
+    roster: list[str]
+
+    name_col_index_start: int = 1
+    name_col_index_end: int = 2
+
+    attendance_col_index_start: int = 3
+    attendance_col_index_end: int = 5
+
+    fuzzy_match_threshold: int = 80
+    tesseract_config: str = "--oem 1 --psm 7"
+    
+
